2bddc5262d1a61725c445b50616b26c2191edef5
unknown <WALZEJON@uw.edu>
2020-02-26 19:32:26 -0800

    change

--- labs/lab12/12a/script.js
+++ labs/lab12/12a/script.js
@@ -1,5 +1,6 @@
 var el = document.getElementById("table_data");
-function oneRow(r, garry) {
+
+function oneRow(garry) {
   var s = "";
   s += "<tr><td>" + garry[0] + "</td>";
   s += "<td>" + garry[1] + "</td>";
@@ -8,14 +9,18 @@ function oneRow(r, garry) {
   s += "</tr>";
   return s;
 }
+
+
 function calcCircleGeometries(radius) {
   const pi = Math.PI;
   var area = pi * radius * radius;
   var circumference = 2 * pi * radius;
   var diameter = 2 * radius;
-  var geometries = [radius, area, circumference, diameter];
+  
+  var geometries = [radius.toFixed(2), area.toFixed(2), circumference.toFixed(2), diameter.toFixed(2)];
   return geometries;
 }
+
 rad1 = calcCircleGeometries(Math.random());
 rad2 = calcCircleGeometries(Math.random());
 rad3 = calcCircleGeometries(Math.random());
